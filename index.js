bf6f213e97cf52d4836d41f07c648b9f9bf68e80
Davis_the_don <davismugoikou@gmail.com>
2024-09-19 11:44:52 +0300

    I made icons work

---
+++ index.js
@@ -0,0 +1,16 @@
+// alert("helooo")
+// let usersectionIcon= document.querySelector('.userH2').innerHTML
+// let runRock = ()=>{
+// usersectionIcon = '&#9994;'
+// }
+// let runPaper = ()=>{
+//     usersectionIcon = '&#9995;' 
+// }
+// let runScissors = ()=>{
+//     usersectionIcon = "&#9996;"
+// }
+let changeItemIcon=(itemReceived)=>{
+    //alert(itemReceived);
+    document.querySelector('.userH2').innerHTML = itemReceived
+
+}
